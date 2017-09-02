1038fe1fc71386719f706b502d2daad8b9ab1f25
jiang1991 <jiang12196@126.com>
2017-09-02 23:31:12 +0800

    first copy

---
+++ underscore.js
@@ -0,0 +1,70 @@
+// Underscore copy code
+// Author by jiang
+
+(function () {
+    var root = this;
+    var previousUnderscore = root._;
+    var
+        ArrayProto = Array.prototype,
+        ObjProto = Object.prototype,
+        FuncProto = Function.prototype;
+    var
+        push = ArrayProto.push,
+        slice = ArrayProto.slice,
+        toString = ObjProto.toString,
+        hasOwnProperty = ObjProto.hasOwnProperty;
+
+    var
+        nativeIsArray = Array.isArray,
+        nativeKeys = Object.keys,
+        nativeBind = FuncProto.bind,
+        nativeCreate = Object.create;
+
+    var Ctor = function () {};
+
+    var _ = function (obj) {
+        if(obj instanceof _)
+            return obj;
+        if(!(this instanceof _)) {
+            return new _(obj);
+        }
+
+        this._wrapped = obj;
+    };
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = _;
+        }
+        exports._ = _;
+    } else {
+        root._ = _;
+    }
+
+    _.VERSION = '1.0.0';
+
+    var optimizeCb = function (func, context, argCount) {
+        if (context === void 0) {
+            return func;
+        }
+        switch (argCount == null ? 3 : argCount) {
+            case 1: return function (value) {
+                return func.call(context, value)
+            };
+            case 2: return function (value, other) {
+                return func.call(context, value, other)
+            };
+
+            case 3: return function (value, index, collection) {
+                return func.call(context, value, index, collection);
+            }
+        }
+
+        return function () {
+            return func.apply(context, arguments);
+        }
+    }
+
+
+
+}.call(this));
