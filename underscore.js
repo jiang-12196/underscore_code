cbf9d51f432214d7fbb7061ea0c16a79b8ce33dd
jiang1991 <jiang12196@126.com>
2017-09-14 22:13:00 +0800

    add sample sortBy

--- underscore.js
+++ underscore.js
@@ -453,6 +453,35 @@
         return shuffled;
     };
 
+    _.sample = function (obj, n, guard) {
+          if ( n == null || guard) {
+              if (!isArrayLike(obj)) obj = _.values(obj);
+              return obj[_.random(obj.length - 1)];
+          }
+
+          return _.shuffle(obj).slice(0, Math.max(0, n));
+    };
+
+    _.sortBy = function (obj, iteratee, context) {
+        iteratee = cb(iteratee, context);
+        return _.pluck(
+            _.map(obj, function (value, index, list) {
+                return {
+                    value: value,
+                    index: index,
+                    criteria: iteratee(value, index, list);
+                }
+            }).sort(function (left, right) {
+                var a = left.criteria;
+                var b = right.criteria;
+                if (a !== b) {
+                    if (a > b || a === void 0) return 1;
+                    if (a < b || b === void 0) return -1;
+                }
+                return left.index - right.index;
+            }), 'value');
+    };
+
     _.random = function (min, max) {
         if (max == null) {
             max = min;
