013068023dbee93e4745e252cdb99e1febdaeaa3
jiang1991 <jiang12196@126.com>
2017-09-21 23:59:59 +0800

    add toArray

--- underscore.js
+++ underscore.js
@@ -511,6 +511,18 @@
          else result[key] = 1;
     });
 
+    _.toArray = function (obj) {
+        if (!obj) return [];
+        if (_.isArray(obj)) return slice.call(obj);
+
+        if (isArrayLike(obj)) return _.map(obj, _.identity);
+        return _.values(obj);
+    };
+
+    _.identity = function (value) {
+        return value;
+    };
+
     _.random = function (min, max) {
         if (max == null) {
             max = min;
