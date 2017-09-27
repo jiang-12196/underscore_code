1860db314f8a25903666e41bdd402b9eb7171905
jiang1991 <jiang12196@126.com>
2017-09-27 23:07:26 +0800

    add range function

--- underscore.js
+++ underscore.js
@@ -309,6 +309,7 @@
     }
 
     _.findIndex = createPredicateIndexFinder(1);
+    _.findLastIndex = createPredicateIndexFinder(-1);
 
     _.findKey = function (obj, predicate, context) {
         predicate = cb(predicate, context);
@@ -682,6 +683,25 @@
               else high = mid;
           }
           return low;
+    };
+
+    _.indexOf = createIndexFinder(1, _.findIndex, _.sortedIndex);
+
+    _.lastIndexOf = createIndexFinder(-1, _.findLastIndex);
+
+    _.range = function (start, stop, step) {
+        if (stop == null) {
+            stop = start || 0;
+            start = 0;
+        }
+        step = step || 1;
+        var length = Math.max(Math.ceil((stop - start) / step), 0);
+
+        var range = Array(length);
+        for (var idx = 0; idx < length; idx++, start += step) {
+            range[idx] = start;
+        }
+        return range;
     };
 
     _.isBoolean = function (obj) {
